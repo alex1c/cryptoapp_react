261a348da2449da8c0e7968515862afe89fc62f3
alex1c <alex1c.spb@gmail.com>
2022-02-15 21:51:21 +0300

    master

--- src/components/NewHomepage.jsx
+++ src/components/NewHomepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import millify from 'millify';
-import { Typography, Row, Col, Statistic } from 'antd';
+//import millify from 'millify';
+import { Typography, Row } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
